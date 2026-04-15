fb26ffb76ee574c0ffcede16dfd400225b232670
BossNatiyut10 <bossbattlefront@gmail.com>
2026-04-15 23:47:10 +0700

    Fix CORS error

--- server.js
+++ server.js
@@ -8,7 +8,21 @@ const giftRoutes = require('./routes/gift');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' })); // Vite default port
+const allowedOrigins = [
+  process.env.FRONTEND_URL || 'http://localhost:5173',];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('CORS policy: Origin not allowed'));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
